669f083218079b81369b03a690bedb8d10ef99a2
taichi <taicloud0803@icloud.com>
2022-06-12 01:49:23 +0900

    Initial Commit

---
+++ TCAPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCAPractice
+//
+//  Created by taichi on 2022/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by taichi on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
