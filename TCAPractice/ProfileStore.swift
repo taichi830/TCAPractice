570309270aa68f740cd06caf7162c043fa4aa5ac
taichi <taicloud0803@icloud.com>
2022-06-15 02:38:38 +0900

    ProfileStoreを記述

--- TCAPractice/ProfileStore.swift
+++ TCAPractice/ProfileStore.swift
@@ -5,4 +5,30 @@
 //  Created by taichi on 2022/06/15.
 //
 
-import Foundation
+import ComposableArchitecture
+
+struct ProfileState: Equatable {
+    private(set) var count = 0
+    private(set) var currentTab: Tab
+    
+    fileprivate mutating func resetCount() {
+        self.count = 0
+        self.currentTab = .counter
+    }
+}
+
+enum ProfileAction: Equatable {
+    case resetCounterButtonTapped
+}
+
+struct ProfileEnvironment {}
+
+let profileReducer = Reducer<ProfileState, ProfileAction, ProfileEnvironment> { state, action, _ in
+    switch action {
+    case .resetCounterButtonTapped:
+        state.resetCount()
+        return .none
+    }
+}
+
+
